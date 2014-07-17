ef037f1f31999e44b978891d1e5aec823844358d
noili <federico.neuman@gmail.com>
2014-07-17 19:22:18 -0300

    Corrected routes.

--- config/routes.rb
+++ config/routes.rb
@@ -23,10 +23,10 @@ Rails.application.routes.draw do
     resources :comments
   end
   
-  get "log_out" => "sessions#destroy", :as => "log_out"
-  get "log_in" => "sessions#new", :as => "log_in"
-  get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  get "log_out" => "sessions#destroy"
+  get "log_in" => "sessions#new"
+  get "sign_up" => "users#new"
+  root :to => "sessions#new"
   resources :users
   resources :sessions
   # Example of regular route:
