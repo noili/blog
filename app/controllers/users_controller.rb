fc9f6e5dba0dfdd6e645a4658d72663834c3e627
noili <federico.neuman@gmail.com>
2014-07-17 19:22:59 -0300

    Removed comments.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
+    #binding.pry
     @user = User.new(params[:user])
     if @user.save
       redirect_to root_url, :notice => "Signed up!"
